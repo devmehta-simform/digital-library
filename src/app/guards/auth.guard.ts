30dc0327caea2705a6ec4eca11ebf1961b5fa622
devmehta-simform <dev.mehta@simformsolutions.com>
2025-06-26 15:09:35 +0530

    feat: add simple error handling in guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,23 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { map } from 'rxjs';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   const routeData = route.data;
-  return authService.getRole().pipe(map((role) => role === routeData['role']));
+  return authService.getRole().pipe(
+    map((role) => {
+      if (role === routeData['role']) return true;
+      else {
+        router.navigate(['']);
+        return false;
+      }
+    }),
+    catchError(() => {
+      router.navigate(['']);
+      return of(false);
+    })
+  );
 };
